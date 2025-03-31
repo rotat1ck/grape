47d59f35bcafee05a523deb1676a2c8f342a6ecb
rotat1ck <rotatick32151@gmail.com>
2025-03-31 16:41:31 +0300

    добавил комментарии в два основных файла

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
-from .config import initConfig
-from .routes.users import users_bp
+from .config import initConfig # <= файл конфигурации, создает базу данных и вытаскивает параметры среды
+# параметры среды определяются в файле .env, его нет на гитхабе в целях безопастности сервера
+from .routes.users import users_bp # <= подключение путей, переменные заканчивающиеся на _bp определяют 
+# какой будет префикс для конечной точки, пример: grape.rotatick.ru/api/users/login, где api/users префикс 
+# для конечной точки login
 
 
 def startApp():
-    app = Flask(__name__)
-    initConfig(app)
+    app = Flask(__name__) # создание переменной приложения
+    initConfig(app) # запуск файла конфигурации config.py
 
-    app.register_blueprint(users_bp, url_prefix='/api/users')
+    app.register_blueprint(users_bp, url_prefix='/api/users') # <= обозначение префикса /api/users
 
-    return app
+    return app # возвращаем объект приложения в main.py для запуска
--- server/main.py
+++ server/main.py
@@ -1,10 +1,10 @@
-from app import startApp
+from app import startApp # <= функция которая создает приложение и подключает все конечные точки
 from app.config import db
 
-app = startApp()
+app = startApp() # <= вызов данной фунции
 
 with app.app_context():
-    db.create_all()
+    db.create_all() # <= создание таблиц в базе данных
 
 if __name__ == '__main__':
-    app.run()
+    app.run() # <= запуск сервера
