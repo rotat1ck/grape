89d261f92263b512789c42e29d4a9ed8d0ff0e64
rotat1ck <rotatick32151@gmail.com>
2025-04-01 17:21:26 +0300

    добавил небольшие изменения модели базы данных, добавил шаблон для /api/notes/

--- grape/src/mainwindow/mainwindow.cpp
+++ grape/src/mainwindow/mainwindow.cpp
@@ -1,10 +1,10 @@
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
-    centralWidget = new QWidget(this); // test
+    centralWidget = new QWidget(this);
     setCentralWidget(centralWidget);
     layout = new QStackedLayout(centralWidget); // <- описание объекта в src/mainwindow/mainwindow.h
-    centralWidget->setLayout(layout);
+
 
     login = new Login(this);
 
@@ -14,6 +14,8 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
         мог отрисовать объект на экране */
 
     layout->setCurrentWidget(login); // <- показ виджета
+
+    setFixedSize(640, 720);
 }
 
 MainWindow::~MainWindow() {
