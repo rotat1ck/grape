e9e08ef97baa9da29248538edf462f75843139f4
rotat1ck <rotatick32151@gmail.com>
2025-04-07 11:18:19 +0300

    закончил внешний вид страницы login

--- grape/src/login/login.cpp
+++ grape/src/login/login.cpp
@@ -3,6 +3,18 @@
 
 Login::Login(QWidget *parent) : QWidget(parent) , ui(new Ui::Login) {
     ui->setupUi(this);
+
+    QIcon icon;
+    icon.addFile(":/img/password.svg",QSize(34,20),QIcon::Normal,QIcon::On);
+    ui->PasswordStateButton->setIcon(icon);
+
+    ClickQLabel* registrationButton = new ClickQLabel(ui->centralwidget);
+    registrationButton->setText("Register");
+    registrationButton->setGeometry(390, 590, 80, 30);
+
+    connect(registrationButton, &ClickQLabel::clicked, this, [](){
+        qDebug() << "CLicked register button";
+    });
 }
 
 Login::~Login()
@@ -10,4 +22,12 @@ Login::~Login()
     delete ui;
 }
 
+void Login::on_PasswordStateButton_clicked() {
+    isPasswordVisible = !isPasswordVisible;
+    if (isPasswordVisible) {
+        ui->PasswordInput->setEchoMode(QLineEdit::Normal);
+    } else {
+        ui->PasswordInput->setEchoMode(QLineEdit::Password);
+    }
+}
 
