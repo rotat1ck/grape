1f555669d9cf21855d3a1941fe5e1223db10fd9c
rotat1ck <rotatick32151@gmail.com>
2025-04-21 11:47:21 +0300

    Изменил пример использования нет кода в src/mainwindow/net-mainwindow.cpp

--- grape/src/mainwindow/net-mainwindow.cpp
+++ grape/src/mainwindow/net-mainwindow.cpp
@@ -4,18 +4,21 @@ void MainWindow::setupNetHandlers() {
     netHandler = new Net();
 
     Net::Result res = netHandler->user->sendLoginRequest("zov@gmail.com", "testtest");
+
     if (!res.isFailure) {
         qDebug() << netHandler->token;
     } else {
         qDebug() << res.message;
     }
 
+    netHandler->tasks->addUserTask("GOIDA");
+
     std::vector<Task> tasks = netHandler->tasks->getUserTasks();
     for (int i = 0; i < tasks.size(); ++i) {
         qDebug() << i << ". " + tasks[i].name;
     }
 
-    netHandler->tasks->deleteUserTask(tasks[3].id);
+    netHandler->tasks->deleteUserTask(tasks[tasks.size() - 1].id);
 
     tasks = netHandler->tasks->getUserTasks();
     for (int i = 0; i < tasks.size(); ++i) {
