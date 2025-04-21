9ab06f6110dc3b8f608b72edd9a382f8a64f1253
rotat1ck <rotatick32151@gmail.com>
2025-04-21 10:52:01 +0300

    Добавил функционал класса tasks, для получения, создания и удаления задач

--- grape/net/dashboard-net/task-net.cpp
+++ grape/net/dashboard-net/task-net.cpp
@@ -42,3 +42,59 @@ std::vector<Task> Tasks::getUserTasks() {
 
     return tasks;
 }
+
+// - - ADDTASK FUNCS - -
+
+Net::Result Tasks::addUserTaskImp(QString content) {
+    netHandler->cl->set_default_headers({{"Authorization", "Bearer " + netHandler->token}});
+
+    std::string endpoint = "/api/tasks/addtask?content=" + content.toUtf8().toStdString();
+    if (auto res = netHandler->cl->Post(endpoint)) {
+        if (res->status != 200) {
+            if (res->status != 502) {
+                json response = json::parse(res->body);
+                return {res->status, response["error"], true};
+            } else {
+                return {502, "Bad gateway", true};
+            }
+        } else {
+            return {200, "Task added", false};
+        }
+    } else {
+        return {-1, "Connection failed", true};
+    }
+}
+
+Net::Result Tasks::addUserTask(QString content) {
+    return netHandler->retryRequest(3, [this, content]() {
+        return addUserTaskImp(content);
+    });
+}
+
+// - - DELETETASK FUNCS - -
+
+Net::Result Tasks::deleteUserTaskImp(int id) {
+    netHandler->cl->set_default_headers({{"Authorization", "Bearer " + netHandler->token}});
+
+    std::string endpoint = "/api/tasks/deletetask/" + std::to_string(id);
+    if (auto res = netHandler->cl->Delete(endpoint)) {
+        if (res->status != 200) {
+            if (res->status != 502) {
+                json response = json::parse(res->body);
+                return {res->status, response["error"], true};
+            } else {
+                return {502, "Bad gateway", true};
+            }
+        } else {
+            return {200, "Task deleted", false};
+        }
+    } else {
+        return {-1, "Connection failed", true};
+    }
+}
+
+Net::Result Tasks::deleteUserTask(int id) {
+    return netHandler->retryRequest(3, [this, id]() {
+        return deleteUserTaskImp(id);
+    });
+}
